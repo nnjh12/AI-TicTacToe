46078f95d5580ccf7c1eb50e6f98401a772f4ffe
Gina <nnjh12@gmail.com>
2022-12-02 15:44:22 -0500

    minor update in experiment

--- tictactoe_experiment.py
+++ tictactoe_experiment.py
@@ -3,6 +3,8 @@
 
 from tictactoe_helpers import *
 from tictactoe_AI import *
+from matplotlib import pyplot as plt
+import numpy as np
 
 # x_strategy and o_strategy should be one of "baseline AI" or "MCTS"
 def experiment(num_simulates: int, _board_size: int, x_strategy, o_strategy):
@@ -12,7 +14,8 @@ def experiment(num_simulates: int, _board_size: int, x_strategy, o_strategy):
     board_size = _board_size
     strategies = (x_strategy, o_strategy)
     state = initial_state(board_size)
-    num_nodes_processed = [] # list of number of nodes processed by MCTS for each game
+    score_track = []
+    num_nodes_track = [] # list of number of nodes processed by MCTS for each game
     while(total_rounds < num_simulates):
         total_rounds += 1
         nodes_processed_per_game = 0 # number of nodes processed by MCTS for one game
@@ -27,19 +30,35 @@ def experiment(num_simulates: int, _board_size: int, x_strategy, o_strategy):
                 nodes_processed_per_game += nodes_processed_per_action
         
         game_score = score(state)
-        num_nodes_processed.append(nodes_processed_per_game)
+        score_track.append(game_score)
+        num_nodes_track.append(nodes_processed_per_game)
         if (game_score == 1): total_score[0] += 1
         if (game_score == -1): total_score[1] += 1
-        print(" X(%s) wins: %s, O(%s) wins: %s, Tie: %s (out of %s games)" 
-            % (x_strategy, total_score[0], o_strategy, total_score[1], total_rounds - sum(total_score), total_rounds))
+        # print(" X(%s) wins: %s, O(%s) wins: %s, Tie: %s (out of %s games)" 
+        #    % (x_strategy, total_score[0], o_strategy, total_score[1], total_rounds - sum(total_score), total_rounds))
         state = initial_state(board_size)
 
     print("----- Results -----")
     print(" X(%s) wins: %s, O(%s) wins: %s, Tie: %s (out of %s games)" 
             % (x_strategy, total_score[0], o_strategy, total_score[1], total_rounds - sum(total_score), total_rounds))
-    print(" number of nodes processed by MCTS over each game: %s" %(num_nodes_processed))
-    print("-------------------")
+    print(" score over each game:\n%s" %(np.array(score_track).reshape(10, 10)))
+    print(" number of nodes processed by MCTS over each game:\n%s" %(np.array(num_nodes_track).reshape(10, 10)))
+    print("-------------------\n")
 
-# TODO: update main for tic tac toe
+# TODO: update histogram
+def histogram(list):
+    return list
+
+# TODO: update main
 if __name__ == "__main__":
     experiment(100, 3, "baseline AI", "MCTS")
+    experiment(100, 4, "baseline AI", "MCTS")
+    experiment(100, 5, "baseline AI", "MCTS")
+    experiment(100, 6, "baseline AI", "MCTS")
+    experiment(100, 7, "baseline AI", "MCTS")
+
+    experiment(100, 3, "MCTS", "baseline AI")
+    experiment(100, 4, "MCTS", "baseline AI")
+    experiment(100, 5, "MCTS", "baseline AI")
+    experiment(100, 6, "MCTS", "baseline AI")
+    experiment(100, 7, "MCTS", "baseline AI")
